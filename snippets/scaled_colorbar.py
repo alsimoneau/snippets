2cefa87da112009e72420974f1f487ef5e312cab
Alexandre Simoneau <alsimoneau@gmail.com>
2022-02-02 13:50:24 -0500

    Added scaled colorbar

---
+++ snippets/scaled_colorbar.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def scaled_colorbar(im, size="5%", pad=0.05):
+    # create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(plt.gca())
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    return plt.colorbar(im, cax=cax)
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    plt.figure()
+    im = plt.imshow(np.arange(100).reshape((10, 10)))
+
+    scaled_colorbar(im)
